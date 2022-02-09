33f32ee4471d940b34017915c6cb61d97c076aab
Axu32 <akseli.hankaniemi@gmail.com>
2022-02-09 21:36:06 +0200

    optional system info

--- src/modules/utils.rs
+++ src/modules/utils.rs
@@ -57,7 +57,7 @@ pub async fn get_ping(ctx: &Context) -> String {
     latency
 }
 
-pub async fn get_sys() -> String{
+pub async fn get_sys(full: Option<bool>) -> String{
     // Get cpu usage, cpu count, memory usage, uptime, rust version, serenity version, and the number of shards
 
     let mut sys = System::new_all();
@@ -65,23 +65,41 @@ pub async fn get_sys() -> String{
     sys.refresh_all();
     
     let memory_usage = format!(
-        "{}B / {}B ({}%)",
+        "{}B / {}B ({:.1}%)",
         bytes_to_human(sys.used_memory()).await,
         bytes_to_human(sys.total_memory()).await,
         sys.used_memory() as f64 / sys.total_memory() as f64 * 100.0
     );
     let cpu_count = format!("{}", sys.processors().len());
-    let mut cpu_usage = String::new();
+    let mut cpu_usage = Vec::new();
     for core in sys.processors() {
-        cpu_usage.push_str(&format!(
-            "\n    {}%",
+        cpu_usage.push(
             core.cpu_usage()
-        ))
+        )
+        
     }
+    let mut cpu_usage_str = String::new();
+    let mut avg = 0_f32;
+    if let Some(is_full) = full{
+        if is_full{
+            for val in &cpu_usage{
+                cpu_usage_str.push_str(&format!("\n{:.1}%", val));
+            }
+        }
+    } else {
+        for val in &cpu_usage {
+            avg += val;
+        }
+        avg /= cpu_usage.len() as f32
+    };
+    if avg != 0_f32 {
+        cpu_usage_str = avg.round().to_string();
+    }
+
     if let Some(process) = sys.process(sys.processes_by_name("sbot").nth(0).unwrap().pid()) {
         let uptime = format!("{} s", &process.run_time());
-        return format!("memory: {}\ncore:{}\ncpu usage: {}\nuptime: {}", memory_usage, cpu_count, cpu_usage, uptime);
+        return format!("memory: {}\ncore:{}\ncpu usage: {}\nuptime: {}", memory_usage, cpu_count, cpu_usage_str, uptime);
     }
     let uptime = "? s".to_string();
-    return format!("memory: {}\ncore:{}\ncpu usage: {}\nuptime: {}", memory_usage, cpu_count, cpu_usage, uptime);
+    return format!("memory: {}\ncore:{}\ncpu usage: {}\nuptime: {}", memory_usage, cpu_count, cpu_usage_str, uptime);
 }
