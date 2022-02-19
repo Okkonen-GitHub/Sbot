59d40b872b423667ac24fb95dcc74462b2760a16
Okko <okko.oikkonen@gmail.com>
2022-02-19 22:44:42 +0200

    bot statuses v2

--- src/modules/activities.rs
+++ src/modules/activities.rs
@@ -3,7 +3,7 @@ use rand::{Rng, thread_rng};
 use chrono::offset::Utc;
 use std::sync::Arc;
 use super::utils::get_sys;
-
+use serenity::model::prelude::OnlineStatus;
 // ShardManager from main.rs
 use crate::ShardManagerContainer;
 
@@ -13,13 +13,17 @@ pub async fn set_status(ctx: Arc<Context>) {
     //* Define activies and choose one of them randomly
     let mut activities = Vec::new();
 
+
     activities.push(Activity::listening("s help".to_string()));
     activities.push(Activity::watching("s help".to_string()));
     activities.push(Activity::playing(Utc::now().to_rfc2822()));
     activities.push(Activity::watching(get_sys(false).await.get("memory_usage").unwrap()));
 
-    let rng = thread_rng().gen_range(0..activities.len());
+    let statuses = [OnlineStatus::Online, OnlineStatus::Idle, OnlineStatus::DoNotDisturb];
 
+    let rng = thread_rng().gen_range(0..activities.len());
+    let status_rng = thread_rng().gen_range(0..statuses.len());
+    let status = statuses[status_rng];
 
     //* Get the bot shards and change the status for each of them
     let data_read = ctx.data.read().await;
@@ -28,6 +32,6 @@ pub async fn set_status(ctx: Arc<Context>) {
     // complicated I know...
     let runners = manager.runners.lock().await;
     for (_id, runner) in runners.iter() {
-        runner.runner_tx.set_activity(Some(activities[rng].clone()));
+        runner.runner_tx.set_presence(Some(activities[rng].clone()), status);
     }
 }
