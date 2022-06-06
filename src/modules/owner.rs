7c79f09db553051d491478e75f19e629d416c2db
Okko <okko.oikkonen@gmail.com>
2022-06-06 19:26:16 +0300

    owner command - quit

--- src/modules/owner.rs
+++ src/modules/owner.rs
@@ -11,10 +11,50 @@ use crate::ShardManagerContainer;
 #[aliases("exit", "shutdown")]
 async fn quit(ctx: &Context, msg: &Message) -> CommandResult {
     let data = ctx.data.read().await;
+    let uid = msg.author.id;
 
     if let Some(manager) = data.get::<ShardManagerContainer>() {
-        msg.reply(ctx, "Shutting down!").await?;
-        manager.lock().await.shutdown_all().await;
+        let resp = msg.reply(ctx, "Are you sure you want to shutdown the bot?").await?;
+
+        match msg.react(ctx, '✅').await {
+            Ok(_) => {
+                let _ = msg.react(ctx, '❌').await;
+            }
+            Err(why) => {
+                msg.reply(ctx, format!("Error: {}", why)).await?;
+            }
+        }
+        loop {
+            let reactions = match msg.reaction_users(ctx, '✅', None, Some(uid)).await {
+                Ok(v) => v,
+                Err(why) => {
+                    msg.reply(ctx, format!("Error: {}", why)).await?;
+                    break;
+                }
+            };
+            for user in reactions {
+                if user.id == uid {
+                    println!("Shutting down... {}", user.name);
+                    manager.lock().await.shutdown_all().await;
+                    break;
+                }
+            }
+            let reactions = match msg.reaction_users(ctx, '❌', None, Some(uid)).await {
+                Ok(v) => v,
+                Err(why) => {
+                    msg.reply(ctx, format!("Error: {}", why)).await?;
+                    break;
+                }
+            };
+            for user in reactions {
+                if user.id == uid {
+                    let _ = resp.delete(ctx).await;
+                    break;
+                }
+            }
+
+        }
+
     } else {
         msg.reply(ctx, "There was a problem getting the shard manager")
             .await?;
