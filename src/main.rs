c428a64539c2dd15a3504215888ab58d0e8e7923
Okko <okko.contact@proton.me>
2025-07-15 22:53:07 +0300

    More "infrastructure"

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 mod modules;
 
-use crate::modules::core::*;
+use crate::modules::{
+    core::*,
+    owner::*,
+};
 
 use std::{
-    collections::{HashSet},
+    collections::HashSet,
     env,
     sync::Arc,
 };
 
-use serenity::prelude::*;
+use serenity::{
+    prelude::*,
+    // model::id::GuildId
+};
 use serenity::{
     async_trait,
-    client::bridge::gateway::{ShardId, ShardManager},
+    client::bridge::gateway::ShardManager,
     framework::standard::{
-        macros::{command, group},
-        CommandResult,
+        macros::group,
         StandardFramework,
     },
     http::Http,
-    model::{
-        channel::{Message},
-        gateway::Ready,
-    },
+    model::gateway::Ready,
 };
 use tokio::sync::Mutex;
 
@@ -29,29 +31,29 @@ use tokio::sync::Mutex;
 
 // add commands to a group!
 #[group]
-#[commands(ping, about)]
+#[commands(ping, about, quit)]
 struct General;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "sping" {
-            println!("Shard {}", ctx.shard_id);
+    // async fn message(&self, ctx: Context, msg: Message) {
+    //     if msg.content == "sping" {
+    //         println!("Shard {}", ctx.shard_id);
 
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error {:?}", why);
-            }
-        }
-    }
+    //         if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+    //             println!("Error {:?}", why);
+    //         }
+    //     }
+    // }
     
     async fn ready(&self, ctx: Context, ready: Ready) {
         let guilds = match ready.user.guilds(ctx).await {
             Ok(v) => v.len().to_string(),
             _ => String::from("?")
         };
-        println!("{} is connected to {} servers",ready.user.name, guilds);
+        println!("{} is connected & total guilds: {} ",ready.user.name, guilds);
     }
 }
 
@@ -86,7 +88,7 @@ async fn main() {
         },
         Err(why) => panic!("Could not access application info: {:?}", why),
     };
-
+    // println!("{:?}", owners);
     let framework = StandardFramework::new()
         .configure(|c| c
         .prefix("s")
@@ -116,29 +118,5 @@ async fn main() {
     }
 }
 
-#[command]
-#[aliases("latency")]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    let latency = {
-        let data_read = ctx.data.read().await;
-        let shard_manager = data_read.get::<ShardManagerContainer>().unwrap();
 
-        let manager = shard_manager.lock().await;
-        let runners = manager.runners.lock().await;
-
-        let runner = runners.get(&ShardId(ctx.shard_id)).unwrap();
-
-        if let Some(duration) = runner.latency {
-            format!("{:.2} ms", duration.as_millis())
-        } else {
-            "? ms".to_string()
-        }
-    };
-    msg.reply(
-        ctx,
-        format!("Bot latency: {}", latency)
-    ).await?;
-
-    Ok(())
-}
 
