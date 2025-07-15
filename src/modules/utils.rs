b02dc9e98c46bda5bfbb60cddf452c0559a90a53
Okko <okko.contact@proton.me>
2025-07-15 22:53:43 +0300

    useless refactoring

--- src/modules/utils.rs
+++ src/modules/utils.rs
@@ -1,8 +1,12 @@
-use std::{collections::HashMap, fs, path::PathBuf, env};
+use std::{collections::{HashMap, HashSet}, fs, path::PathBuf, env};
 
 use crate::ShardManagerContainer;
 
-use serenity::client::{bridge::gateway::ShardId, Context};
+use serenity::{
+    http::Http,
+    model::id::UserId,
+    client::{bridge::gateway::ShardId, Context},
+};
 
 use sysinfo::{System, SystemExt, ProcessorExt, ProcessExt};
 
@@ -122,6 +126,31 @@ pub async fn get_sys(full: bool) -> HashMap<&'static str, String> {
     }
 
     sys_info
+}
+
+// Returns a tuple containing a hashset of bot owners' ids and the bot's id
+pub async fn get_owners(token: &str) -> (HashSet<UserId>, UserId) {
+    let http = Http::new_with_token(&token);
+
+    // fetch your bot's owners and id
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                for member in team.members {
+                    owners.insert(member.user.id);
+                }
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+    return (owners, bot_id);
 }
 
 #[cfg(debug_assertions)]
